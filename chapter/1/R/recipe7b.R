7b77046c5a88c9f469add168dd3ad13c26c2001d
Alan Beale <alan.beale@chicagotrading.com>
2011-11-24 12:05:08 -0600

    add image version of 7b heatmap

--- chapter/1/R/recipe7b.R
+++ chapter/1/R/recipe7b.R
@@ -1,24 +1,35 @@
 source("comboPlot.R")
 
 # import data
-genes <-read.csv("data/genes.csv")
-
+genes <-read.csv("../data/genes.csv")
+main.title="Gene Correlation Matrix"
+rownames(genes) <-colnames(genes)
 #define common text
 
 #main.text="Unit Sales in the month\nof January 2010"
 #ylab.text="Number of units sold"
 
-base.recipe.2a <-function() {
+base.recipe.7b <-function() {
+  image(x=1:ncol(genes),
+        y=1:nrow(genes),
+        z=t(as.matrix(genes)),
+        axes=FALSE,
+        xlab="",  ylab="",
+        main=main.title)
+  axis(1,at=1:ncol(genes),labels=colnames(genes),col="white",
+       las=2,cex.axis=0.8)
+  axis(2,at=1:nrow(genes),labels=rownames(genes),col="white",
+       las=1,cex.axis=0.8)
 
 }
 
 # ggplot data shaping
 cgenes<-colnames(genes)
 
+genes2<-genes
+genes2$var<-factor(cgenes,levels=cgenes)
 
-genes$var<-factor(cgenes,levels=rev(cgenes))
-
-genes.melt<-melt.data.frame(genes,id.vars=c("var"))
+genes.melt<-melt.data.frame(genes2,id.vars=c("var"))
 
 
 ## ggplot
@@ -41,9 +52,15 @@ genes.melt<-melt.data.frame(genes,id.vars=c("var"))
 
 #   adjust space around graph
 #  opts(plot.margin = unit(c(2, 2, 2, 1), "lines"))
+
+# use expand argument to remove expanded margin space
+
 plot7.2 <-ggplot(genes.melt) +
   geom_tile(aes(x=variable,y=var,fill=value)) +
+  scale_fill_gradientn(colours = heat.colors(12))+
+  scale_x_discrete("",expand=c(0,0)) +
+  scale_y_discrete("",expand=c(0,0))+
   theme_bw() +
-  opts(axis.text.x = theme_text(angle=45))
+  opts(axis.text.x = theme_text(angle=90))
 
 #doComboPlot(doBasePlot=base.recipe.2a,gplot=plot2.1)
