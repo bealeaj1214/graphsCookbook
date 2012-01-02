cc13d52bb10d8dd07378feef7418f77fc237cfe3
Alan Beale <alan.beale@chicagotrading.com>
2012-01-02 11:27:13 -0600

    1st draft of plotting global data by countries on a world map

--- chapter/09/R/recipe01.R
+++ chapter/09/R/recipe01.R
@@ -3,6 +3,10 @@
 require(maps)
 require(WDI)
 require(RColorBrewer)
+require(ggplot2)
+
+# common text
+map.title="CO2 emissions (kg per 2000 US$ of GDP)"
 
 # how to do it
 colors<- brewer.pal(7,"PuRd")
@@ -37,29 +41,31 @@ for (i in 1:length(x$measure))
   sc[i]=as.character(sd$col[findInterval(x$measure[i],
       sd$values)])
 
-#2- column layout with color scale to right of map
-layout(matrix(data=c(2,1), nrow=1, ncol=2),widths=c(8,1),
-       heights=c(8,1))
 
 # Color Scale first
 breaks <-sd$values
+book.recipe.01a<-function() {
+  #2- column layout with color scale to right of map
+  layout(matrix(data=c(2,1), nrow=1, ncol=2),widths=c(8,1),
+         heights=c(8,1))
 
-par(mar = c(5,1,10,7), oma=c(0.2,0.2,0.2,0.2), mex=0.25)
+  par(mar = c(5,1,10,7), oma=c(0.2,0.2,0.2,0.2), mex=0.25)
+  
+  image(x=1,y=0:length(breaks),z=t(matrix(breaks))*1.001,
+        col=colors[1:length(breaks) -1],axes=FALSE,
+        breaks=breaks,xlab="",ylab,xaxt="n")
 
-image(x=1,y=0:length(breaks),z=t(matrix(breaks))*1.001,
-      col=colors[1:length(breaks) -1],axes=FALSE,
-      breaks=breaks,xlab="",ylab,xaxt="n")
+  axis(side=4,at=0:(length(breaks) -1),
+       labels=round(breaks),col="white",las=1)
 
-axis(side=4,at=0:(length(breaks) -1),
-     labels=round(breaks),col="white",las=1)
+  abline(h=c(1:length(breaks)),col="white",lwd=2,xpd=F)
 
-abline(h=c(1:length(breaks)),col="white",lwd=2,xpd=F)
+  #Map
+  map(col=sc,fill=TRUE,lty="blank")
 
-#Map
-map(col=sc,fill=TRUE,lty="blank")
-
-map(add=TRUE,col="gray",fill=FALSE)
-title("CO2 emissions (kg per 2000 US$ of GDP)")
+  map(add=TRUE,col="gray",fill=FALSE)
+  title(map.title)
+}
 
 
 
@@ -73,5 +79,27 @@ world.df.co<-subset(world.df,ucountry %in% toupper(w$country))
 w.aug<-w[,c(1,4)]
 names(w.aug) <-c("country","measure")
 w.aug$country<-toupper(w.aug$country)
-test<-merge(world.df.co,w.aug,by.x="ucountry",by.y="country",all.y=FALSE)
+world.df.merge<-merge(world.df.co,w.aug,by.x="ucountry",by.y="country",all.y=FALSE)
 
+world.df.co2<-subset(world.df.merge,!is.na(measure))
+#merge operation didn't preserve order
+world.df.co2<-world.df.co2[order(world.df.co2$order),]
+world.df.co2$codedMeas<-factor(findInterval(world.df.co2$measure,sd$values))
+
+# use of manual fill scale
+#http://learnr.wordpress.com/2009/04/15/ggplot2-qualitative-colour-palettes/
+
+plot01.1 <-ggplot(data=world.df.co2) +
+  geom_polygon(aes(long,lat,group=group,fill=codedMeas),
+               colour="gray")+
+  geom_polygon(data=world.df,aes(long,lat,group=group),
+               colour="gray",fill="transparent",legend=FALSE)+
+  scale_fill_manual("C02 score",values=colors) +
+  theme_bw() +
+  opts(title =map.title,
+       plot.margin = unit(c(4, 0.5, 6, 0.5), "lines"))
+
+
+ggRecipe01a<-function() {
+  print(plot01.1)
+}
